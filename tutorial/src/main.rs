0e3d4f55c925accd6b811f5a779da212976fb2cd
Robert Rees <shudderfix+git@gmail.com>
2026-03-13 23:22:14 +0000

    Adds keyboard controls to the program

--- tutorial/src/main.rs
+++ tutorial/src/main.rs
@@ -3,8 +3,34 @@ use macroquad::prelude::*;
 #[macroquad::main("Tutorial Game")]
 async fn main() {
     println!("Hello, world!");
+
+    let mut x = screen_width() / 2.0;
+    let mut y = screen_height() /2.0;
+
+    let velocity = 1.0;
+
     loop {
         clear_background(SKYBLUE);
-        next_frame().await
+
+        if is_key_down(KeyCode::Down) {
+            y+=velocity;
+        }
+
+        if is_key_down(KeyCode::Up) {
+            y-=velocity;
+        }
+
+        if is_key_down(KeyCode::Right) {
+            x+=velocity
+        }
+
+        if is_key_down(KeyCode::Left) {
+            x-=velocity;
+        }
+
+        draw_circle(x, y, 16.0, YELLOW);
+
+
+        next_frame().await;
     }
 }
